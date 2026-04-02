267e3eb754f85a4f7755fd0d86f9ba3b43d98c6b
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-02 14:56:23 +0300

    feat(data): add ExpectedData class with test constants

---
+++ src/test/java/com/haritonov/practicum/data/ExpectedData.java
@@ -0,0 +1,9 @@
+package com.haritonov.practicum.data;
+
+public class ExpectedData {
+
+    public static final String PAGE_URL = "https://practice-automation.com/form-fields/";
+
+    public static final int PASSWORD_MIN_LENGTH = 8;
+    public static final int PASSWORD_MAX_LENGTH = 12;
+}
