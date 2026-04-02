fbd9d62a88fab2e9cedb282d3e1de005897bda69
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-02 17:26:20 +0300

    feat(data): add constants for drink, color and like options

--- src/test/java/com/haritonov/practicum/data/ExpectedData.java
+++ src/test/java/com/haritonov/practicum/data/ExpectedData.java
@@ -4,6 +4,11 @@ public class ExpectedData {
 
     public static final String PAGE_URL = "https://practice-automation.com/form-fields/";
 
+    public static final String DRINK_MILK = "Milk";
+    public static final String DRINK_COFFEE = "Coffee";
+    public static final String COROL_YELLOW = "Yellow";
+    public static final String LIKE_AUTOMATION_YES = "Yes";
+
     public static final int PASSWORD_MIN_LENGTH = 8;
     public static final int PASSWORD_MAX_LENGTH = 12;
 }
