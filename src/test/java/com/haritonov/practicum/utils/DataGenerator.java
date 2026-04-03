de142b453e5d7f29b35a6bcb34d48f354dfd8aa7
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-03 11:31:55 +0300

    refactor(utils): remove unused method and add JavaDoc

--- src/test/java/com/haritonov/practicum/utils/DataGenerator.java
+++ src/test/java/com/haritonov/practicum/utils/DataGenerator.java
@@ -5,24 +5,39 @@ import com.haritonov.practicum.data.ExpectedData;
 
 import java.util.Random;
 
+/**
+ * Утилитарный класс для генерации случайных тестовых данных.
+ * <p>
+ * Использует библиотеку JavaFaker для создания валидных email, паролей и имен.
+ * </p>
+ */
 public class DataGenerator {
 
     private static final Faker faker = new Faker();
     private static final Random random = new Random();
 
+    /**
+     * Генерирует случайное имя (First Name + Last Name) с использованием Faker
+     * @return случайное имя
+     */
     public static String getRandomName() {
         return faker.name().firstName() + " " + faker.name().lastName();
     }
 
+    /**
+     * Генерирует случайный пароль указанной длины
+     * @return случайный пароль
+     */
     public static String getRandomPassword() {
         return faker.internet().password(ExpectedData.PASSWORD_MIN_LENGTH, ExpectedData.PASSWORD_MAX_LENGTH);
     }
 
+    /**
+     * Генерирует случайный email адрес
+     * @return случайный email
+     */
     public static String getRandomEmail() {
         return faker.internet().emailAddress();
     }
 
-    public static int getRandomNumber(int minValue, int maxValue) {
-        return random.nextInt(minValue, maxValue);
-    }
 }
